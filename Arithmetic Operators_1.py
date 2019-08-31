d80dcf1ff3c56addaea38d80ddc69657fde84c75
Nishad Patil <nishadvp@mitaoe.ac.in>
2019-09-01 00:57:03 +0530

    Add files via upload

---
+++ Arithmetic Operators_1.py
@@ -0,0 +1,15 @@
+# Task
+# Read two integers from STDIN and print three lines where:
+
+#     The first line contains the sum of the two numbers.
+#     The second line contains the difference of the two numbers (first - second).
+#     The third line contains the product of the two n
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+
+
